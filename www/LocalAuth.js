60293671f0462d82f67194280b5991ec8b09d41d
Max Andreola <max@kingsidedesign.com>
2015-11-30 07:36:45 -0600

    adding methods to JS

--- www/LocalAuth.js
+++ www/LocalAuth.js
@@ -1,5 +1,24 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "LocalAuth", "coolMethod", [arg0]);
+exports.initialize = function(appKey, success, error) {
+    exec(success, error, "LocalAuth", "initialize", [appKey]);
 };
+
+exports.user = function(email, password, success, error) {
+	var params = [];
+	if (email) {
+		params.push(email);
+		if (password) {
+			params.push(password);
+		}
+	}
+	exec(success, error, "LocalAuth", "user", params);
+}
+
+exports.isAvailable = function() {
+	exec(success, error, "LocalAuth", "isAvailable", []);
+}
+
+exports.authenticate = function(success, error) {
+	exec(success, error, "LocalAuth", "authenticate", []);
+}
