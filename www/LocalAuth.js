cbf9cd2536980b977f9079dfe2b7fb151f4e7ad8
Chandrababu <chandrababu.kamani.ctr@sabre.com>
2016-05-12 10:44:08 -0500

    updated android plugin changes

--- www/LocalAuth.js
+++ www/LocalAuth.js
@@ -1,44 +1,29 @@
-var exec = require('cordova/exec');
-
-exports.user = function(email, password, success, error) {
-	var params = [];
-	if (email) {
-		params.push(email);
-		if (password) {
-			params.push(password);
-		}
+var localAuth = {
+	authenticate: function(string){
+		return new Promise(function(resolve, reject) {
+			cordova.exec(
+				resolve,
+				reject,
+				"LocalAuth",
+				"authenticate",
+				[string,// Array of arguments to pass to the Java class
+                clientId: "hello world",//alias for app package name
+                clientSecret: "foo bar"//client secret for android key store
+            	]
+			);
+		});
+	},
+	supported: function(){
+		return new Promise(function(resolve, reject) {
+			cordova.exec(
+				resolve,
+				reject,
+				"LocalAuth",
+				"capable",
+				[]
+			);
+		});
 	}
-	if (params.length != 2) {
-		exec(success, error, "LocalAuth", "clearUser");
-	} else {
-		exec(success, error, "LocalAuth", "user", params);
-	}
 };
 
-exports.checkAvailable = function() {
-	return new Promise(function(resolve, reject) {
-		exec(resolve, reject, "LocalAuth", "isAvailable", []);
-	}).catch(function(e) {
-		console.log('fail', e);
-	});
-};
-
-exports.enrolled = function() {
-	return new Promise(function(resolve, reject) {
-		exec(resolve, reject, "LocalAuth", "enrolled", []);
-	}).catch(function(e) {
-		console.log('fail', e);
-	});
-};
-
-exports.authenticate = function(success, error) {
-	return new Promise(function(resolve, reject) {
-		exec(resolve, reject, "LocalAuth", "authenticate", []);
-	}).catch(function(response) {
-		try {
-			console.log(JSON.parse(response));
-		} catch(e) {
-			console.log(e, response);
-		}
-	});
-};
+module.exports = localAuth;
